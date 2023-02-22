7df86b9649bfb2d034a799fc3b418db56f7aecbd
adamb <adam.blmomquist98@gmail.com>
2023-02-22 16:17:18 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require("express")
+const app = express()
+require("dotenv").config()
+const PORT = process.env.PORT || 3000
+const fs = require('fs')
+const bodyParser = require("body-parser")
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.json())
+
+let resultList = {results:[{Hand: "78s",Result: "Win"},{Hand: "AA",Result: "Loss"},{Hand: "AQo",Result: "Loss"}]}
+
+var allinList = JSON.parse(fs.readFileSync( __dirname + "/data.json", 'UTF-8'));
+
+app.get("/", (req,res) =>{
+    res.sendFile(__dirname + "/index.html")   
+})
+
+app.get("/allin/:id", (req, res) =>{
+    console.log(req.params.id)
+    var id = +req.params.id
+    console.log(id)
+    var allin = allinList.find(a => a.ID == id);
+    console.log(allin)
+    console.log(allinList)
+    res.send(allin);
+})
+app.get("/results", (req, res) =>{
+    res.send(resultList)
+})
+app.get("/remove", (req, res) =>{
+    resultList.results.pop()
+})
+
+app.get("/addResult", (req, res) =>{
+    res.sendFile(__dirname + "/resultform.html")
+})
+
+app.post("/addResult", (req, res) =>{
+    let hand = req.body.Hand
+    let result = req.body.Result
+
+    resultList.results.push({Hand: `${hand}`, Result: `${result}`})
+})
+
+app.listen(PORT, () =>{
+
+})
+
